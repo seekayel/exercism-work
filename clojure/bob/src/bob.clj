93d218e9bbc66b862f69710fa757d6919f2e5ea7
Kam Lasater <kam.lasater@heavywater.com>
2020-03-18 23:10:11 -0400

    Checkpoint

--- clojure/bob/src/bob.clj
+++ clojure/bob/src/bob.clj
@@ -1,5 +1,12 @@
-(ns bob)
+(ns bob
+  (:require [clojure.string :as str]))
 
-(defn response-for [s] ;; <- arglist goes here
-  ;; your code goes here
-)
+
+
+(defn response-for [in]
+  (let [s (str/trim in)]         
+    (cond
+      (and (= s (str/upper-case s) (str/ends-with? s "?"))) "Calm down, I know what I'm doing!"
+      (= s (str/upper-case s)) "Whoa, chill out!"
+      (str/ends-with? s "?") "Sure."
+      :else "Whatever.")))
