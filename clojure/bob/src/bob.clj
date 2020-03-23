711ed065035ecc27b8b9b2b7f83761aae1acdb29
Kam Lasater <kam.lasater@heavywater.com>
2020-03-22 22:07:02 -0400

    Solution time, learn all dem re-finds and match parens

--- clojure/bob/src/bob.clj
+++ clojure/bob/src/bob.clj
@@ -5,8 +5,14 @@
 
 (defn response-for [in]
   (let [s (str/trim in)]
+    ; (println s)
+    ; (println (and (= s (str/upper-case s) (str/ends-with? s "?"))))
+    ; (println (= s (str/upper-case s)))
+    ; (println (str/ends-with? s "?"))
     (cond
-      (and (= s (str/upper-case s) (str/ends-with? s "?"))) "Calm down, I know what I'm doing!"
-      (or (= s (str/upper-case s)) (str/ends-with? s "!")) "Whoa, chill out!"
+      (= s "") "Fine. Be that way!"
+      (and (= s (str/upper-case s)) (str/ends-with? s "?") (re-find #"[A-Z]" s)) "Calm down, I know what I'm doing!"
       (str/ends-with? s "?") "Sure."
+      (re-matches #"[A-Z ]+" s) "Whoa, chill out!"
+      (and (= s (str/upper-case s)) (str/ends-with? s "!")) "Whoa, chill out!"
       :else "Whatever.")))
